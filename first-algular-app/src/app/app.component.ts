3765d1efcdceaef88ad978fb055877266b0bb8a0
udashsanam <udashsanam@gmail.com>
2025-04-23 14:26:17 +0545

    add: older ng if and ng for

--- first-algular-app/src/app/app.component.ts
+++ first-algular-app/src/app/app.component.ts
@@ -4,11 +4,12 @@ import {HeaderComponent} from './header/header.component';
 import {UserComponent} from './user/user.component';
 import {DUMMY_USERS} from './dummy-users';
 import {TasksComponent} from './tasks/tasks.component';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
