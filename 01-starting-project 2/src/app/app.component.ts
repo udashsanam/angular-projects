0ca8fb28e3ae9aea6a9dda140b7c12c5b110a441
udashsanam <udashsanam@gmail.com>
2025-05-14 16:36:57 +0545

    add: shared component dashboard-item and using ngcontent

--- 01-starting-project 2/src/app/app.component.ts
+++ 01-starting-project 2/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
-import {ServerStatusComponent} from "./server-status/server-status.component";
-import {TrafficComponent} from "./traffic/traffic.component";
-import {SupportTicketsComponent} from "./support-tickets/support-tickets.component";
+import {ServerStatusComponent} from "./dashboard/server-status/server-status.component";
+import {TrafficComponent} from "./dashboard/traffic/traffic.component";
+import {SupportTicketsComponent} from "./dashboard/support-tickets/support-tickets.component";
+import {DashboardItemComponent} from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ import {SupportTicketsComponent} from "./support-tickets/support-tickets.compone
     HeaderComponent,
     ServerStatusComponent,
     TrafficComponent,
-    SupportTicketsComponent
+    SupportTicketsComponent,
+    DashboardItemComponent
   ]
 })
 export class AppComponent {
