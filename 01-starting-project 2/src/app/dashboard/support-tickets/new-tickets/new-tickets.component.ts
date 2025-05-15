15eb9e86b6a012184d83e6bc1eb230e40ed759df
udashsanam <udashsanam@gmail.com>
2025-05-15 17:28:01 +0545

    diff css for component

--- 01-starting-project 2/src/app/dashboard/support-tickets/new-tickets/new-tickets.component.ts
+++ 01-starting-project 2/src/app/dashboard/support-tickets/new-tickets/new-tickets.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {ButtonComponent} from "../../../shared/button/button.component";
+import {ControlComponent} from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-tickets',
   standalone: true,
   imports: [
     FormsModule,
-    ButtonComponent
+    ButtonComponent,
+    ControlComponent
   ],
   templateUrl: './new-tickets.component.html',
   styleUrl: './new-tickets.component.css'
