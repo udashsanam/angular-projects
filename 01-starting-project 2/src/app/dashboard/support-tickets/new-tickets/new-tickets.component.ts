a91eabec6727fc5fc03e626844a4255b9b18e10d
udashsanam <udashsanam@gmail.com>
2025-05-16 16:54:40 +0545

    add: view child and view childer to gt value from html template

--- 01-starting-project 2/src/app/dashboard/support-tickets/new-tickets/new-tickets.component.ts
+++ 01-starting-project 2/src/app/dashboard/support-tickets/new-tickets/new-tickets.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, viewChild, ViewChild, ViewChildren} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {ButtonComponent} from "../../../shared/button/button.component";
 import {ControlComponent} from "../../../shared/control/control.component";
+import {elementAt} from "rxjs";
 
 @Component({
   selector: 'app-new-tickets',
@@ -15,11 +16,15 @@ import {ControlComponent} from "../../../shared/control/control.component";
   styleUrl: './new-tickets.component.css'
 })
 export class NewTicketsComponent {
-
+// @ViewChild('form') form ?: ElementRef<HTMLFormElement>;
+private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  @ViewChildren(ControlComponent) controls!: ElementRef<HTMLInputElement>[];
   onSubmit(title:string, request:string) {
     console.log('Submitted');
     console.log(title);
     console.log(request);
+    this.form().nativeElement.reset();
+    console.log(this.controls);
   }
 
 }
