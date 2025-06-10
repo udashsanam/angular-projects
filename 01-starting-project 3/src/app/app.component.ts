f5ec395ab366bbb09a7e298ae1d110e514c3add4
udashsanam <udashsanam@gmail.com>
2025-06-10 13:43:31 +0545

    add: initial project

---
+++ 01-starting-project 3/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import {Component, computed, inject} from '@angular/core';
+
+import { AuthComponent } from './auth/auth.component';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import {AuthService} from "./auth/auth.service";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [AuthComponent, LearningResourcesComponent, NgIf],
+})
+export class AppComponent {
+
+  private  authService = inject(AuthService);
+
+  isAdmin = computed(() => this.authService.activePermission()=== 'admin');
+
+}
