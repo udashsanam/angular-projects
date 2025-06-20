a853720af72dd3970a8ceb4130b34d1520112a15
udashsanam <udashsanam@gmail.com>
2025-06-20 13:23:04 +0545

    add: strtural direct complete with core concept

--- 01-starting-project 3/src/app/auth/auth.directive.ts
+++ 01-starting-project 3/src/app/auth/auth.directive.ts
@@ -1,17 +1,23 @@
-import {Directive, effect, input} from '@angular/core';
+import {Directive, effect, inject, input, TemplateRef, viewChild, ViewContainerRef} from '@angular/core';
 import {Permission} from "./auth.model";
 import {AuthService} from "./auth.service";
 
 @Directive({
   selector: '[appAuth]',
-  standalone: true
+  standalone: true,
+
 })
 export class AuthDirective {
   userType = input.required<Permission>({alias: 'appAuth'});
+  // getting html inside the ng template
+  private templateRef = inject(TemplateRef);
+  private viewContainerRef = inject(ViewContainerRef);
   constructor(private authService: AuthService) {
     effect(() => {
       if(this.authService.activePermission() === this.userType()){
-        console.log("show element")
+        console.log("show element");
+        // this will render the templateRef content
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
       }else {
         console.log("do not show element")
       }
